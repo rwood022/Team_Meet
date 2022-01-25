edeeff0e08279cb3402fdab07fa0ecbd5f3f6ba3
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-25 01:32:44 -0500

    last changes to test file

--- test/team.test.js
+++ test/team.test.js
@@ -1,10 +1,26 @@
+// const { iteratee } = require("lodash");
+// const { hasUncaughtExceptionCaptureCallback } = require("process");
 const Employee = require("../lib/employee");
 
-describe("Employee class", () => {
+describe("Employee", () => {
     //  prompts the user with questions from employee class (name, id, email)
+    describe("getName", () => {
+        it("should return an object containing a 'string' when called with the 'new' keyword", () => {
+            const obj = new Employee("Tech.co");
+
+            expect("string" in obj).toEqual(true);
+        });
+    });
+    describe("getId", () => {
+        it("should return an object containing a 'number' property when called with the 'new' keyword", () => {
+            const obj = new Employee();
+
+            expect("number" in obj).toEqual(true);
+        });
+    });
+
 
 })
 
-describe("Manager class", () => {
-    
-})
+
+
