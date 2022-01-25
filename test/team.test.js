4e73a7c8f1f77d3a0b62f19cf8b929d976f4dfbb
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-24 21:05:36 -0500

    fixed code for engineer

--- test/team.test.js
+++ test/team.test.js
@@ -1,5 +1,10 @@
 const Employee = require("../lib/employee");
 
 describe("Employee class", () => {
+    //  prompts the user with questions from employee class (name, id, email)
 
+})
+
+describe("Manager class", () => {
+    
 })
