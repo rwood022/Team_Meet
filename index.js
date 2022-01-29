05946569e34c4738e6587755846b8649fab337d1
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-29 17:58:58 -0500

    fixed switch code

--- index.js
+++ index.js
@@ -9,22 +9,18 @@ const { inherits } = require("util");
 const team = [];
 
 
-//     return inquirer.prompt([
 
-//     ]);
-// };
-const promptQuestions = () => {
     const inputTeam = () => {
         inquirer.prompt([
             {
                 type: "list",
                 name: "menu",
                 message: "Select another employee:",
-                choices: ["manager", "engineer", "intern, done"]
+                choices: ["manager", "engineer", "intern", "done"]
             } 
- //switch structure, allows the excution of a specific code block - https://www.educative.io/edpresso/how-to-use-the-switch-statement-in-javascript
-        ]).then((choice) => {
-            switch(choice.menu) {
+ //Switch Structure, "allows the execution of a specific code block depending upon the evaluation of an expression."- https://www.educative.io/edpresso/how-to-use-the-switch-statement-in-javascript
+        ]).then((choices) => {
+            switch(choices.menu) {
                 case "manager":
                     managerQuestions();
                     break;
@@ -34,8 +30,8 @@ const promptQuestions = () => {
                 case "intern":
                     internQuestions();
                     break;
-                    default:
-                        init();
+                case "done":
+                    init();
             }
         })
     }
@@ -62,217 +58,82 @@ const promptQuestions = () => {
                 name: "officeNumber",
                 message: "what is their office number?",
             }
+            //Tutor helped with .then functions
         ]).then((managerAnswers) => {
             console.log("managerAnswers", managerAnswers);
             const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber, managerAnswers.menu);
             team.push(manager);
-
-        
+            inputTeam();
         })
     }
     
-const engineerQuestions = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the engineers name?",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their I.D.?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email?",
-        },
-        {
-            type: "input",
-            name: "gitHub",
-            message: "what is their GitHub?",
-        },
-        {
-            type: "list",
-            name: "menu",
-            message: "Select another employee:",
-            choices: ["manager", "engineer", "intern"]
-        }
-    ]).then((engineerAnswers) => {
-        console.log("engineerAnswers", engineerAnswers);
-        const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.gitHub);
-        team.push(engineer);
+    const engineerQuestions = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the engineers name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their I.D.?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email?",
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "what is their GitHub?",
+            }
+        ]).then((engineerAnswers) => {
+            console.log("engineerAnswers", engineerAnswers);
+            const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.gitHub);
+            team.push(engineer);
+            inputTeam();
+        })
+    }
+
+    const internQuestions = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the interns name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their I.D.?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email?",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Where did they go to school?",
+            }
+        ]).then((internAnswers) => {
+            console.log("internAnswers", internAnswers);
+            const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+            team.push(intern);
+            inputTeam();
+        })
     
-       
-    })
-}
+    }
 
-const internQuestions = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the interns name?",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their I.D.?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email?",
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "Where did they go to school?",
-        },
-        {
-            type: "list",
-            name: "menu",
-            message: "Select another employee:",
-            choices: ["manager", "engineer", "intern"]
-        }
-
-    ]).then((internAnswers) => {
-        console.log("internAnswers", internAnswers);
-        const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
-        team.push(intern);
-        
-        
-    })
-    
-}}
-const generateTeam = team => {
-
-    // create the manager html
-    const generateManager = manager => {
-        return`
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
-        `;
+    function init() {
+        inputTeam()
+        .then((answers) => fs.writeFileSync("index.html", generateTeam(answers)))
+        .then(() => console.log("index.html has been written"))
+        .catch((err) => console.error(err));
     };
 
-    // create the html for engineers
-    const generateEngineer = engineer => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
-        `;
-    };
 
-    // create the html for interns
-    const generateIntern = intern => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
-        `;
-    };
-
-    const html = [];
-
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
-        .join("")
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Intern")
-        .map(intern => generateIntern(intern))
-        .join("")
-    );
-
-    return html.join("");
-
-}
-
-// export function to generate entire page
-module.exports = team => {
-
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
-            </div>
-        </div>
-    </div>
-</body>
-</html>
-    `;
-};
-
-
-
-// function init() {
-    managerQuestions()
-    engineerQuestions()
-    internQuestions()
-//         .then((answers) => fs.writeFlitSync("index.html",generateHTML(answers)))
-//         .then(() => console.log("index.html has been written"))
-//         .catch((err) => console.error(err));
-// };
-
-// init();
+inputTeam();
